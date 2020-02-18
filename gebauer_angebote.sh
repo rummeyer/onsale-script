5ea631f899b4301d1fc31a6fba58160bc8d89a77
Oliver Rummeyer <rummeyer@users.noreply.github.com>
2020-02-18 08:41:55 +0100

    Some changes

--- gebauer_angebote.sh
+++ gebauer_angebote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # calc week number with leading zero
-WEEK=$(printf "%02d" "`date +%V`")
+WEEK=$(printf "`date +%V`")
 if [ "$WEEK" -eq "52" ]; then
   WEEK="01"
 fi
@@ -11,31 +11,33 @@ TEMPDIR="/home/pi/.tmp_$WEEK"
 if [ -d "${TEMPDIR}" ]; then
   rm -rf "${TEMPDIR}"
 fi
+echo "Tempdir is ${TEMPDIR}"
 mkdir "${TEMPDIR}"
 cd "${TEMPDIR}"
 
+PATTERN="*[-_]KW${WEEK}[-_.]*pdf"
+
 # get files from website
-wget -r -A.pdf https://www.gebauer-markt.de/
+wget --ignore-case -r -A "${PATTERN}" https://www.gebauer-markt.de/
 
 # send special offers via email
 echo "Sending PDF via E-Mail..."
 # search pdf
-find . -iname "Gebauer*[-_]KW${WEEK}[-_]*.pdf" -exec mpack -s "GEBAUER ANGEBOTE - Prospekt KW${WEEK}" {} oliver@rummeyer.com \;
+find . -iname "${PATTERN}" -exec mpack -s "GEBAUER ANGEBOTE - Prospekt KW${WEEK}" {} oliver@rummeyer.de \;
 
 # send alert if favorite keywords found
 IFS=$'\n'
 KEYWORDS="Schweitzers Schüümli
-Seitenbacher Müsli
-Almighurt
-Bonne Maman
 Ensinger
-Live Fresh Shot"
+Live Fresh Shot
+Innocent
+Garden Gourmet"
 
 for KEYWORD in ${KEYWORDS}; do
   echo "Processing '${KEYWORD}' alert..."
-  find . -iname "Gebauer*[-_]KW${WEEK}[-_]*.pdf" -print0 | xargs -I{} -0 pdftotext -q {} - | grep -i -C 10 "$KEYWORD" | mail --exec 'set nonullbody' -s "GEBAUER ANGEBOTE - Reduziert '${KEYWORD}'" oliver@rummeyer.com
+  find . -iname "${PATTERN}" -print0 | xargs -I{} -0 pdftotext -q {} - | grep -i -C 10 "$KEYWORD" | mail --exec 'set nonullbody' -s "GEBAUER ANGEBOTE - Reduziert '${KEYWORD}'" oliver@rummeyer.de sarah@rummeyer.de
 done
 
 # remove all downloaded files
 echo "Deleting temp dir..."
-rm -rf "${TEMPDIR}"
+#rm -rf "${TEMPDIR}"
